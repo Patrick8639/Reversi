f26c47eeb36da1961ef782d3d3d4325243e071d1
Patrick8639 <planz@ordinasoft.ch>
2021-02-02 16:59:51 +0100

    Cleaned the application created by the template

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Reversi.Server
 {
@@ -18,10 +11,11 @@ namespace Reversi.Server
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+      Host
+        .CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+          {
+            webBuilder.UseStartup<Startup>();
+          });
   }
 }
